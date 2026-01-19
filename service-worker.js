ccc7f8742943e5c850760c93a22083583fb70387
Anna.S <haruya.9674@gmail.com>
2026-01-19 19:00:12 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,4 @@
-/* VERSION: FINAL_SW_2026_01_20 */
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  // 常にネットワーク優先（保存破壊防止）
-  e.respondWith(fetch(e.request));
-});
+/* FINAL_SW_NO_CACHE */
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("fetch", e => e.respondWith(fetch(e.request)));
